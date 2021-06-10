2d339d0ba6bc1774df79dc00c0eceabfe3e9c174
Thiago Schnr <thiago.schnr@mercadolivre.com>
2021-06-10 12:39:41 -0300

    first commit

---
+++ src/main/java/com/mercadolivre/desafioquality/DesafioqualityApplication.java
@@ -0,0 +1,13 @@
+package com.mercadolivre.desafioquality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioqualityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioqualityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mercadolivre/desafioquality/DesafioqualityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadolivre.desafioquality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioqualityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
